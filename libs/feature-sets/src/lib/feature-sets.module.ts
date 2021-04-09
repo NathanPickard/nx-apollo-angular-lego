977e41ecd0313d601a4018725d95a87c1ae374df
Nathan Pickard <nathan.pickard87@gmail.com>
2021-04-09 14:30:32 -0700

    Generated set form and set list components

--- libs/feature-sets/src/lib/feature-sets.module.ts
+++ libs/feature-sets/src/lib/feature-sets.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SetListComponent } from './set-list/set-list.component';
+import { SetFormComponent } from './set-form/set-form.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule],
+  declarations: [
+    SetListComponent,
+    SetFormComponent
+  ],
+  exports: [
+    SetListComponent,
+    SetFormComponent
+  ],
 })
 export class FeatureSetsModule {}
