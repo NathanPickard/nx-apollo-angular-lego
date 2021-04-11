6990648fb7866ec4ac4d6edce056dc7ae179be84
Nathan Pickard <nathan.pickard87@gmail.com>
2021-04-10 22:35:47 -0700

    Mapping results from the SetListGQL service

--- libs/feature-sets/src/lib/set-list/set-list.component.ts
+++ libs/feature-sets/src/lib/set-list/set-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { Set, SetListGQL } from '@nx-apollo-angular-lego/data-access';
+
 
 @Component({
   selector: 'nx-apollo-angular-lego-set-list',
   templateUrl: './set-list.component.html',
   styleUrls: ['./set-list.component.css']
 })
-export class SetListComponent implements OnInit {
+export class SetListComponent {
 
-  constructor() { }
+  sets$: Observable<Set[]>;
 
-  ngOnInit(): void {
+  constructor(private setListGQL: SetListGQL) { 
+    this.sets$ = this.setListGQL.watch().valueChanges.pipe(map((result) => result.data.allSets));
   }
 
 }
