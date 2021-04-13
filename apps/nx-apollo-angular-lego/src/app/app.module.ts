c61f577bbfdaaa38d071e4aaf5efa77a8cdcfcf3
Nathan Pickard <nathan.pickard87@gmail.com>
2021-04-12 17:07:09 -0700

    Imported set-form and set-list components into app component

--- apps/nx-apollo-angular-lego/src/app/app.module.ts
+++ apps/nx-apollo-angular-lego/src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FeatureSetsModule } from '@nx-apollo-angular-lego/feature-sets';
+
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, GraphQLModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    GraphQLModule,
+    FeatureSetsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
